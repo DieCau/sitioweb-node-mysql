19ca8cdec04ab14181c6c81571000a0fc975bfdc
DiCa! <diecau20@gmail.com>
2021-11-04 23:49:02 -0300

    Se realizo rutas y vistas

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,19 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
-/* GET home page. */
+/* GET home page(index.ejs). */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+/* GET Section(nosotros.ejs). */
+router.get('/nosotros', function(req, res, next) {
+  res.render('nosotros', { title: 'Nosotros somos... ' });
+});
+
+/* GET Section(productos.ejs). */
+router.get('/productos', function(req, res, next) {
+  res.render('productos', { title: 'Nuestros productos...' });
 });
 
 module.exports = router;
