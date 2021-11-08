50648efa9b887a545bf0c5c97010dc62398f3d7d
DiCa! <diecau20@gmail.com>
2021-11-07 22:34:02 -0300

    Se modifico el footer y nosotros

--- routes/index.js
+++ routes/index.js
@@ -3,12 +3,12 @@ const router = express.Router();
 
 /* GET home page(index.ejs). */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Tech Learning' });
 });
 
 /* GET Section(nosotros.ejs). */
 router.get('/nosotros', function(req, res, next) {
-  res.render('nosotros', { title: 'Nosotros somos... ' });
+  res.render('nosotros', { title: 'Acerca de Nosotros... ' });
 });
 
 module.exports = router;
